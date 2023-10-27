54273780ef75f9765e70822a31d749641e9b2bc9
digitalmonk08 <144095649+digitalmonk08@users.noreply.github.com>
2023-10-27 11:56:16 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,29 +6,30 @@
         "PWD" => "adminSql@1234" 
     );
     //Establishes the connection
-   //  $value = $_GET["name"];
-   //  echo "name";
-   //  echo ($value);
-   //  echo "changes hoye hun";
-   //  echo "yes mam ho gye";
-   // echo "done";
+    $lg_email = $_GET["lg_email"];
+    $lg_password = $_GET["lg_password"];
+    $lg_incoming_msg = $_GET["lg_incoming_msg"];
+    $lg_AI_msg = $_GET["lg_AI_msg"];
+    $lg_timestamp = $_GET["lg_timestamp"];
+
     $conn = sqlsrv_connect($serverName, $connectionOptions);
-    $tsql= "SELECT * FROM kareena";
-    $getResults= sqlsrv_query($conn, $tsql);
-    echo ($conn);
-    echo ($getResults);
-    echo ("Reading data from table" . PHP_EOL);
-    if ($getResults == FALSE)
-        echo (sqlsrv_errors());
-    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-echo ($row['customer_id'] . " " . $row['email'] . " " . $row['incoming_msg'] . " " . $row['AI_msg'] .  PHP_EOL);
-    }
+//     $tsql= "SELECT * FROM kareena";
+//     $getResults= sqlsrv_query($conn, $tsql);
+//     echo ($conn);
+//     echo ($getResults);
+//     echo ("Reading data from table" . PHP_EOL);
+//     if ($getResults == FALSE)
+//         echo (sqlsrv_errors());
+//     while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+// echo ($row['customer_id'] . " " . $row['email'] . " " . $row['incoming_msg'] . " " . $row['AI_msg'] .  PHP_EOL);
+//     }
 
-//     $tmgl = "INSERT INTO kareena (customer_id, email, incoming_msg, AI_msg, category, mytimestamp)
-//    VALUES ('54', 'testw@gmail.com', 'hello', 'hii', 'clinical', '26-10-23')";
+    $tmgl = "INSERT INTO login (lg_email, lg_password, lg_incoming_msg, lg_AI_msg, lg_timestamp)
+   VALUES ('$lg_email', '$lg_password', '$lg_incoming_msg', '$lg_AI_msg', '$lg_timestamp')";
 
-//  $getResult= sqlsrv_query($conn, $tmgl);
+echo "Insert Done";
+ $getResult= sqlsrv_query($conn, $tmgl);
 
-//     sqlsrv_free_stmt($getResults);
-//     sqlsrv_free_stmt($getResult);
+    sqlsrv_free_stmt($getResults);
+    sqlsrv_free_stmt($getResult);
 ?>
