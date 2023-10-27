6d87cb38624f1a6c391b02dd33f8a6a2958989e7
digitalmonk08 <144095649+digitalmonk08@users.noreply.github.com>
2023-10-27 11:15:08 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,23 +6,23 @@
         "PWD" => "adminSql@1234" 
     );
     //Establishes the connection
-    $value = $_GET["name"];
-    echo "name";
-    echo ($value);
-    echo "changes hoye hun";
-    echo "yes mam ho gye";
-   echo "done";
-//     $conn = sqlsrv_connect($serverName, $connectionOptions);
-//     $tsql= "SELECT * FROM kareena";
-//     $getResults= sqlsrv_query($conn, $tsql);
-//     echo ($conn);
-//     echo ($getResults);
-//     echo ("Reading data from table" . PHP_EOL);
-//     if ($getResults == FALSE)
-//         echo (sqlsrv_errors());
-//     while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-// echo ($row['customer_id'] . " " . $row['email'] . " " . $row['incoming_msg'] . " " . $row['AI_msg'] .  PHP_EOL);
-//     }
+   //  $value = $_GET["name"];
+   //  echo "name";
+   //  echo ($value);
+   //  echo "changes hoye hun";
+   //  echo "yes mam ho gye";
+   // echo "done";
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    $tsql= "SELECT * FROM kareena";
+    $getResults= sqlsrv_query($conn, $tsql);
+    echo ($conn);
+    echo ($getResults);
+    echo ("Reading data from table" . PHP_EOL);
+    if ($getResults == FALSE)
+        echo (sqlsrv_errors());
+    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+echo ($row['customer_id'] . " " . $row['email'] . " " . $row['incoming_msg'] . " " . $row['AI_msg'] .  PHP_EOL);
+    }
 
 //     $tmgl = "INSERT INTO kareena (customer_id, email, incoming_msg, AI_msg, category, mytimestamp)
 //    VALUES ('54', 'testw@gmail.com', 'hello', 'hii', 'clinical', '26-10-23')";
