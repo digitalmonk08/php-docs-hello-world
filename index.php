f9afd53c90b7c00a5e7c4ed939117608b647f1c3
digitalmonk08 <144095649+digitalmonk08@users.noreply.github.com>
2023-10-27 12:31:20 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,15 +8,24 @@
     //Establishes the connection
 
     $conn = sqlsrv_connect($serverName, $connectionOptions);
-if(isset($_GET["lg_email"] and $_GET["lg_password"] and $_GET["lg_incoming_msg"] and $_GET["lg_AI_msg"] and $_GET["lg_timestamp"]))
-{
-    //**********LOGIN PARAMETER*************
+
+//**********LOGIN PARAMETER*************
     $lg_email = $_GET["lg_email"];
     $lg_password = $_GET["lg_password"];
     $lg_incoming_msg = $_GET["lg_incoming_msg"];
     $lg_AI_msg = $_GET["lg_AI_msg"];
     $lg_timestamp = $_GET["lg_timestamp"];
-    
+
+    //**********SIGNUP PARAMETER*************
+    $sg_email = $_GET["sg_email"];
+    $sg_password = $_GET["sg_password"];
+    $sg_incoming_msg = $_GET["sg_incoming_msg"];
+    $sg_AI_msg = $_GET["sg_AI_msg"];
+    $sg_timestamp = $_GET["sg_timestamp"];
+
+
+if (isset($lg_email, $lg_password, $lg_incoming_msg, $lg_AI_msg, $lg_timestamp)) 
+{
     $login_insert = "INSERT INTO login (lg_email, lg_password, lg_incoming_msg, lg_AI_msg, lg_timestamp)
    VALUES ('$lg_email', '$lg_password', '$lg_incoming_msg', '$lg_AI_msg', '$lg_timestamp')";
 
@@ -25,14 +34,8 @@ if(isset($_GET["lg_email"] and $_GET["lg_password"] and $_GET["lg_incoming_msg"]
     echo "login insert done";
 }
 
-if(isset($_GET["sg_email"] and $_GET["sg_password"] and $_GET["sg_incoming_msg"] and $_GET["sg_AI_msg"] and $_GET["sg_timestamp"]))
+if (isset($sg_email, $sg_password, $sg_incoming_msg, $sg_AI_msg, $sg_timestamp)) 
 {
-    //**********SIGNUP PARAMETER*************
-    $sg_email = $_GET["sg_email"];
-    $sg_password = $_GET["sg_password"];
-    $sg_incoming_msg = $_GET["sg_incoming_msg"];
-    $sg_AI_msg = $_GET["sg_AI_msg"];
-    $sg_timestamp = $_GET["sg_timestamp"];
     
   $signup_insert = "INSERT INTO signup (sg_email, sg_password, sg_incoming_msg, sg_AI_msg, sg_timestamp)
    VALUES ('$sg_email', '$sg_password', '$sg_incoming_msg', '$sg_AI_msg', '$sg_timestamp')";
