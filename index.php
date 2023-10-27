41d254fc0864b91625e8547df9de13665e846738
digitalmonk08 <144095649+digitalmonk08@users.noreply.github.com>
2023-10-27 12:09:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,30 +6,33 @@
         "PWD" => "adminSql@1234" 
     );
     //Establishes the connection
+    //**********LOGIN PARAMETER*************
     $lg_email = $_GET["lg_email"];
     $lg_password = $_GET["lg_password"];
     $lg_incoming_msg = $_GET["lg_incoming_msg"];
     $lg_AI_msg = $_GET["lg_AI_msg"];
     $lg_timestamp = $_GET["lg_timestamp"];
 
-    $conn = sqlsrv_connect($serverName, $connectionOptions);
-//     $tsql= "SELECT * FROM kareena";
-//     $getResults= sqlsrv_query($conn, $tsql);
-//     echo ($conn);
-//     echo ($getResults);
-//     echo ("Reading data from table" . PHP_EOL);
-//     if ($getResults == FALSE)
-//         echo (sqlsrv_errors());
-//     while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-// echo ($row['customer_id'] . " " . $row['email'] . " " . $row['incoming_msg'] . " " . $row['AI_msg'] .  PHP_EOL);
-//     }
 
-    $tmgl = "INSERT INTO login (lg_email, lg_password, lg_incoming_msg, lg_AI_msg, lg_timestamp)
+//**********SIGNUP PARAMETER*************
+    $sg_email = $_GET["sg_email"];
+    $sg_password = $_GET["sg_password"];
+    $sg_incoming_msg = $_GET["sg_incoming_msg"];
+    $sg_AI_msg = $_GET["sg_AI_msg"];
+    $sg_timestamp = $_GET["sg_timestamp"];
+
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+
+    $login_insert = "INSERT INTO login (lg_email, lg_password, lg_incoming_msg, lg_AI_msg, lg_timestamp)
    VALUES ('$lg_email', '$lg_password', '$lg_incoming_msg', '$lg_AI_msg', '$lg_timestamp')";
 
-echo "Insert Done";
- $getResult= sqlsrv_query($conn, $tmgl);
+    $login_Result= sqlsrv_query($conn, $login_insert);
+    sqlsrv_free_stmt($login_Result);
 
-    sqlsrv_free_stmt($getResults);
-    sqlsrv_free_stmt($getResult);
+  $signup_insert = "INSERT INTO signup (sg_email, sg_password, sg_incoming_msg, sg_AI_msg, sg_timestamp)
+   VALUES ('$sg_email', '$sg_password', '$sg_incoming_msg', '$sg_AI_msg', '$sg_timestamp')";
+
+    $signup_result= sqlsrv_query($conn, $signup_insert);
+    sqlsrv_free_stmt($signup_result);
+ echo "signup Insert Done";
 ?>
