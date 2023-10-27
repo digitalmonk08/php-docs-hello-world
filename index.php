f091ff599279391a3e98de53488fb6b32525925b
digitalmonk08 <144095649+digitalmonk08@users.noreply.github.com>
2023-10-27 15:15:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -33,14 +33,14 @@
 
     $tsql= "SELECT * FROM login WHERE lg_email='$lg_email'";
     $getResults= sqlsrv_query($conn, $tsql);
-    echo ($getResults);
-    echo ("Reading data from table" . PHP_EOL);
     if ($getResults == FALSE)
         echo (sqlsrv_errors());
     while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
 echo ($row['lg_customer_id'] . " " . $row['lg_email'] . " " . $row['lg_password'] . " " . $row['lg_incoming_msg'] .  PHP_EOL);
     }
-   
+       if ($row['lg_email'] == $lg_email){
+           echo "Email Already Exist";
+    }
     sqlsrv_free_stmt($getResults);
    
 
