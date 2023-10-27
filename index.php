dee22589193b95080b3f141110d1b083440516c8
digitalmonk08 <144095649+digitalmonk08@users.noreply.github.com>
2023-10-27 13:14:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -30,37 +30,58 @@
     $category= $_GET["category"];
     $mytimestamp = $_GET["mytimestamp"];
 
-
+//Email Chek In login
 if (isset($lg_email, $lg_password, $lg_incoming_msg, $lg_AI_msg, $lg_timestamp)) 
 {
+$email_chek = "SELECT * FROM login WHERE `email` = '$lg_email'";
+$getResults= sqlsrv_query($conn, $email_chek);
+
+if ($getResults->num_rows > 0) {
+        
+    sqlsrv_free_stmt($login_Result);
+    echo "Already Exist";
+    
+} else {
     $login_insert = "INSERT INTO login (lg_email, lg_password, lg_incoming_msg, lg_AI_msg, lg_timestamp)
    VALUES ('$lg_email', '$lg_password', '$lg_incoming_msg', '$lg_AI_msg', '$lg_timestamp')";
 
     $login_Result= sqlsrv_query($conn, $login_insert);
     sqlsrv_free_stmt($login_Result);
     echo "login Successfully";
+    }
 }
 
-if (isset($sg_email, $sg_password, $sg_incoming_msg, $sg_AI_msg, $sg_timestamp)) 
-{
+
+// if (isset($lg_email, $lg_password, $lg_incoming_msg, $lg_AI_msg, $lg_timestamp)) 
+// {
+//     $login_insert = "INSERT INTO login (lg_email, lg_password, lg_incoming_msg, lg_AI_msg, lg_timestamp)
+//    VALUES ('$lg_email', '$lg_password', '$lg_incoming_msg', '$lg_AI_msg', '$lg_timestamp')";
+
+//     $login_Result= sqlsrv_query($conn, $login_insert);
+//     sqlsrv_free_stmt($login_Result);
+//     echo "login Successfully";
+// }
+
+// if (isset($sg_email, $sg_password, $sg_incoming_msg, $sg_AI_msg, $sg_timestamp)) 
+// {
     
-  $signup_insert = "INSERT INTO signup (sg_email, sg_password, sg_incoming_msg, sg_AI_msg, sg_timestamp)
-   VALUES ('$sg_email', '$sg_password', '$sg_incoming_msg', '$sg_AI_msg', '$sg_timestamp')";
+//   $signup_insert = "INSERT INTO signup (sg_email, sg_password, sg_incoming_msg, sg_AI_msg, sg_timestamp)
+//    VALUES ('$sg_email', '$sg_password', '$sg_incoming_msg', '$sg_AI_msg', '$sg_timestamp')";
 
-    $signup_result= sqlsrv_query($conn, $signup_insert);
-    sqlsrv_free_stmt($signup_result);
- echo "signup Successfully";
-}
+//     $signup_result= sqlsrv_query($conn, $signup_insert);
+//     sqlsrv_free_stmt($signup_result);
+//  echo "signup Successfully";
+// }
 
-if (isset($email, $incoming_msg, $AI_msg, $category, $mytimestamp)) 
-{
+// if (isset($email, $incoming_msg, $AI_msg, $category, $mytimestamp)) 
+// {
     
-  $history_insert = "INSERT INTO ID_1001 (email, incoming_msg, AI_msg, category, mytimestamp)
-   VALUES ('$email', '$incoming_msg', '$AI_msg', '$category', '$mytimestamp')";
+//   $history_insert = "INSERT INTO ID_1001 (email, incoming_msg, AI_msg, category, mytimestamp)
+//    VALUES ('$email', '$incoming_msg', '$AI_msg', '$category', '$mytimestamp')";
 
-    $history_result= sqlsrv_query($conn, $history_insert);
-    sqlsrv_free_stmt($history_result);
- echo "History Successfully";
-}
+//     $history_result= sqlsrv_query($conn, $history_insert);
+//     sqlsrv_free_stmt($history_result);
+//  echo "History Successfully";
+// }
     
 ?>
